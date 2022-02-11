94ae33708beda758072947df0fd3a0f12c118537
NItin5Sharma <nitinseo5@gmail.com>
2022-02-11 13:21:51 +0530

    Initial commit

---
+++ src/com/company/Array.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class Array {
+
+   // public static void main (String args[]){
+
+        // Scanner sc=new Scanner(System.in);
+        // int str=sc.nextInt();
+
+        // int number[] = new int[];
+
+        static void Logical_AND(int a, int b){
+//Enter your code here
+                if(a <= 10 && b >= 10){
+                        System.out.println("true");
+                }else{
+                        System.out.println("flase");
+                }
+
+
+        }
+        static void Logical_OR(int a, int b){
+//Enter your code here
+                if(a <= 10 || b >= 10){
+                        System.out.println("true");
+                }else{
+                        System.out.println("flase");
+                }
+        }
+
+        static void Logical_NOT(int a, int b){
+//Enter your code here
+                if(a <= 10 != b >= 10){
+                        System.out.println("true");
+                }else{
+                        System.out.println("flase");
+                }
+        }
+    }
+
+
---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int arr[]=new int[]{1,2,3,4,5};
+
+        for(int i=0; i< arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    } }
+
+
+
+
+
